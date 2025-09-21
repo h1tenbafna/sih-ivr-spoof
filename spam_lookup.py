9221e8b77a949c02de19b771b8d5776f842bab99
Hiten Bafna <87855947+h1tenbafna@users.noreply.github.com>
2025-09-21 19:35:37 +0530

    Update spam_lookup.py

--- spam_lookup.py
+++ spam_lookup.py
@@ -3,7 +3,6 @@ def lookup(test_number):
     flag = 0
     book = open_workbook("new.xls") #xlsx   
     sheet = book.sheet_by_index(0)
-    #test_number = 7083022822
     for row in range(sheet.nrows):
         #print('HELLO')
         if sheet.cell(row, 0).value == test_number:
